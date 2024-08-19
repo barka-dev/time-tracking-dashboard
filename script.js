08fee94d0a2a24b51e4109d77da3b9f1304fed78
Abdel <abdelbarka96@gmail.com>
2024-08-19 15:47:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,9 @@ const fetch_data = (period) => {
                     <section>
                         <div>
                             <strong>${info.title}</strong>
-                            <img src="./images/icon-ellipsis.svg"/>
+                            <svg width="21" height="5" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M2.5 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Z" fill="#BBC0FF" fill-rule="evenodd"/>
+                            </svg>
                         </div>
                         <div>
                             <h2>${info.timeframes[period].current}hrs</h2>
